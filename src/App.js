3d8cb75488007579dfe2a32886465789c1da11a6
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-12-27 16:26:27 +0600

    coustom colors generated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+<button className="btn btn-secondary">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,23 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "blue",
+          "primary-focus": "mediumblue",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
